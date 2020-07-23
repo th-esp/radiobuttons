a639de116bd25a46ad32b6419c646f866dfdfcf2
tobias <tobias-esp-2020@herzkes.de>
2020-07-23 20:05:25 +0200

    Extract functions from setup

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,26 @@
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
 
+#define HOSTNAME "radiobuttons"
+#define OTA_PASSWORD "private"
 #define WLAN_SSID   "Will be redefined"
 #define WLAN_PASSWORD "in next include"
 #include "WLAN_Luftb.hh" // not in git
 
-void setup() {
+void setup_wifi() {
   WiFi.mode(WIFI_STA);
-  WiFi.hostname("radiobuttons");
+  WiFi.hostname(HOSTNAME);
   WiFi.begin(WLAN_SSID, WLAN_PASSWORD);
-  ArduinoOTA.begin();
+}
+
+void setup_over_the_air_updates() {
+    ArduinoOTA.setHostname(HOSTNAME);
+    ArduinoOTA.begin();
+}
+
+void setup() {
+  setup_wifi();
+  setup_over_the_air_updates();
 }
 
 void loop() {
