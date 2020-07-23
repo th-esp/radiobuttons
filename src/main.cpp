9583e37b94850a36dc15670fe92d29a42f9b745b
tobias <tobias-esp-2020@herzkes.de>
2020-07-23 20:13:49 +0200

    Enable pullups

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@
 #include "WLAN_Luftb.hh" // not in git
 
 void setup_wifi() {
+  WiFi.persistent(false);
   WiFi.mode(WIFI_STA);
   WiFi.hostname(HOSTNAME);
   WiFi.begin(WLAN_SSID, WLAN_PASSWORD);
@@ -20,7 +21,18 @@ void setup_over_the_air_updates() {
     ArduinoOTA.begin();
 }
 
+char button_states[5] = "0000";
+unsigned long button_times[4] = {0,0,0,0};
+const char * const button_functions[] = {"turnoff","louder","softer","forward"};
+void setup_buttons() {
+  pinMode(D1, INPUT_PULLUP);
+  pinMode(D2, INPUT_PULLUP);
+  pinMode(D3, INPUT_PULLUP);
+  pinMode(D4, INPUT_PULLUP);
+}
+
 void setup() {
+  setup_buttons();
   setup_wifi();
   setup_over_the_air_updates();
 }
