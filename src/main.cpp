9a50aad07877bb3955c408e5a6aaf230b55f7ff4
tobias <tobias-esp-2020@herzkes.de>
2020-07-23 19:24:04 +0200

    Add basic OTA scaffold

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,20 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
+#define WLAN_SSID   "Will be redefined"
+#define WLAN_PASSWORD "in next include"
+#include "WLAN_Luftb.hh" // not in git
 
 void setup() {
-  // put your setup code here, to run once:
+  WiFi.mode(WIFI_STA);
+  WiFi.hostname("radiobuttons");
+  WiFi.begin(WLAN_SSID, WLAN_PASSWORD);
+  ArduinoOTA.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  ArduinoOTA.handle();
+  delay(5);
 }
