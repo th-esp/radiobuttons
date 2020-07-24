9a6a983ba548dc3a8c2634397b5ed577ab827d0f
tobias <tobias-esp-2020@herzkes.de>
2020-07-24 23:13:56 +0200

    Update states and debounce the buttons

--- include/buttons.hh
+++ include/buttons.hh
@@ -1,19 +1,95 @@
 #ifndef BUTTONS_HH
 #define BUTTONS_HH
 #include <Arduino.h>
+
+enum button_state_t : char {
+    PRESSED='1',
+    UNPRESSED='0',
+    PRESSING='P',
+    RELEASING='R'
+};
+
+/// Repeating sequence of button states is
+/// UNPRESSED -> PRESSING -> PRESSED -> RELEASING -> ...(repeat).
+/// @return the next state that follows the current state
+/// @param current_state state to compute the next state from
+button_state_t next_state(button_state_t current_state);
+
+/// type for pin identifiers and pi states
+typedef uint8_t pin_t;
+/// Type for durations and clock times in milliseconds
+typedef unsigned long int milliseconds_t;
+
+/** The button class keeps track of the button's state and does debouncing
+ * in software.  Responds to state changes of a push button connected to
+ * a digital input. */
 class button_t {
 public:
-    char state = '0';
-    const uint8_t pin;
-    unsigned long time = 0;
-    const String label;
-    button_t(uint8_t pin, const String & label);
-    void handle();
+    /// debounce duration is fixed.
+    static constexpr milliseconds_t debounce_duration = 40;
+    /// last known state of the button
+    button_state_t state = UNPRESSED;
+    /// the ARDUINO pin ID to read from, e.g. D1
+    const pin_t pin;
+    /// which pin state, when stable, corresponds to PRESSED
+    const pin_t pressed_pin_state = LOW;
+    /// Instant when the state changed last time
+    milliseconds_t last_changed_time = 0;
+    /// Label of the button, used for MQTT messages
+    String label;
+
+    /// Initialize new button object.
+    /// @param pin The ARDUINO PIN ID of the digital pin to which the button
+    ///            is connected. Constructor configures a pullup resistor for
+    ///            the pin and sets it to input mode.  The physical pushbutton
+    ///            will short the pin to ground when pressed.
+    /// @param label Human-readable label to return from handle() when the
+    ///              button is pressed.
+    button_t(pin_t pin, const String & label);
+
+    /// Manages button state update including debouncing.
+    /// @return This button's label when a new button press happens.
+    /// @return Empty string otherwise.
+    String handle();
+
+    /// @pre May only be called when the button state is PRESSED or UNPRESSED.
+    /// @return true when the current electrical state does not match its
+    ///         stored state.
+    /// @return false otherwise.
+    bool is_state_change_starting() const;
+
+    /// @pre May only be called when the button state is PRESSED or UNPRESSED.
+    /// To be called when a button press or a button release has just started.
+    /// Advances the button's state to PRESSING or RELEASING.  Starts the
+    /// debouncing timer.
+    void init_debounce();
+
+    /// @pre May only be called when the button state is PRESSED or UNPRESSED.
+    /// Determines if a button press or button release is starting, and if yes,
+    /// starts debouncing.
+    /// @return This button's label when a new button press starts.
+    /// @return Empty string otherwise.
+    String handle_stable_state();
+
+    /// @pre May only be called when the button state is PRESSING or RELEASING.
+    /// Checks how much time has passed since the beginning of the debouncing
+    /// and if the time exceeds debounce_duration, advances the state of this
+    /// button from PRESSING to PRESSED or from RELEASEING to UNPRESSED.
+    /// @return Empty string.
+    String handle_debounce();
+
+
 };
+
+/// Container of multiple button objects.
 class buttons_t {
 public:
-    button_t buttons[4] = {{D1,"turnoff"},{D2,"louder"},{D3,"softer"},{D4,"forward"}};
+    /// Array with all buttons that this object manages.
+    button_t buttons[4] = {{D5,"turnoff"},{D4,"louder"},{D3,"softer"},{D8,"forward"}};
     buttons_t() = default;
-    void handle();
+    /// Updates the states of all buttons.
+    /// @return A vector with the labes with all buttons that experience a new
+    ///         button press.  The vector will be empty when is newly pressed.
+    std::vector<String> handle();
 };
 #endif
--- src/buttons.cpp
+++ src/buttons.cpp
@@ -1,5 +1,14 @@
 #include "buttons.hh"
 
+button_state_t next_state(button_state_t current_state) {
+  switch (current_state) {
+  case UNPRESSED: return PRESSING;
+  case PRESSING:  return PRESSED;
+  case PRESSED:   return RELEASING;
+  case RELEASING: return UNPRESSED;
+  }
+}
+
 button_t::button_t(uint8_t pin, const String & label)
   : pin(pin)
   , label(label)
@@ -7,9 +16,52 @@ button_t::button_t(uint8_t pin, const String & label)
   pinMode(pin, INPUT_PULLUP);
 }
 
-void button_t::handle() {}
+bool button_t::is_state_change_starting() const {
+  bool stored_state_is_pressed = state == PRESSED;
+  bool actual_state_is_pressed = digitalRead(pin) == pressed_pin_state;
+  return stored_state_is_pressed != actual_state_is_pressed;
+}
 
-void buttons_t::handle() {
-  for (auto & button : buttons)
-    button.handle();
+void button_t::init_debounce()
+{
+  state = next_state(state);
+  last_changed_time = millis();
+}
+
+String button_t::handle_stable_state() {
+  if (is_state_change_starting()) {
+    init_debounce();
+    // publish as a button press.
+    if (state == PRESSING) 
+      return label;
+  }
+  return "";
+}
+
+String button_t::handle_debounce() {
+  if (millis() - last_changed_time > debounce_duration) {
+    state = next_state(state);
+  }
+  return "";
+}
+
+String button_t::handle() {
+  switch (state) {
+  case UNPRESSED: case PRESSED:
+    return handle_stable_state();
+  case PRESSING: case RELEASING:
+    handle_debounce();
+    break;
+  }
+  return "";
+}
+
+std::vector<String> buttons_t::handle() {
+  std::vector<String> detected_events;
+  for (auto & button : buttons) {
+    String event = button.handle();
+    if (event.length() > 0)
+      detected_events.push_back(event);
+  }
+  return detected_events;
 }
